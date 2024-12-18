3e901aa2c10c76b9e037130f386c35405fe25b63
James Tansiri <tansirijames@gmail.com>
2024-12-18 09:33:14 +0700

    refactor: move lesson tag to course directory data file

--- src/courses/basic-python/basic-python.11tydata.js
+++ src/courses/basic-python/basic-python.11tydata.js
@@ -2,7 +2,6 @@ module.exports = {
 	course_id: "basic-python",
 	course_name: "Basic Python for Beginners",
 	course_slug: "basic-python",
-	tags: ["lesson"], // This is set so that all subdirectories are tagged as lesson
 	eleventyComputed: {
 		title: (data) => {
 			if (data.tags.indexOf("lesson") !== -1) {
--- src/courses/courses.11tydata.js
+++ src/courses/courses.11tydata.js
@@ -1,3 +1,4 @@
 module.exports = {
 	layout: "layouts/lesson.njk",
+	tags: ["lesson"], // This is set so that all subdirectories are tagged as lesson
 };
