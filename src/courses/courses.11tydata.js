41c29475a1b05eb3e99e99375abaf33a5ec95b31
James Tansiri <tansirijames@gmail.com>
2024-12-21 07:11:21 +0700

    feat: display language dropdown above code editor

--- src/courses/courses.11tydata.js
+++ src/courses/courses.11tydata.js
@@ -9,5 +9,9 @@ module.exports = {
 				return data.title;
 			}
 		},
+		has_multiple_non_common_file_groups: (data) => {
+			if (!data.file_groups) return undefined;
+			return data.file_groups.filter((group) => !group.common).length > 1;
+		},
 	},
 };
