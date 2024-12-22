5bfe6f8206b658bb6cccb6e9f9bdebd208bc5002
James Tansiri <tansirijames@gmail.com>
2024-12-22 10:08:35 +0700

    fix: full path for monaco js and ts language worker

--- _data/env.js
+++ _data/env.js
@@ -1,3 +1,9 @@
 require("dotenv").config();
 
-module.exports = process.env;
+module.exports = {
+	...process.env,
+	base_url:
+		process.env.DEVELOPMENT === "true"
+			? "http://localhost:8080"
+			: "https://www.pythonexpert.dev",
+};
